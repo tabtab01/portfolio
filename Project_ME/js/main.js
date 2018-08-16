98fe05e6efe6c73aea9ce4884c73066a08a49b27
Tabitha Greiger <tgeiger@rv-clt-tgeiger.redventures.net>
2018-08-15 23:08:11 -0400

    js and design updates

--- Project_ME/js/main.js
+++ Project_ME/js/main.js
@@ -1,3 +1,10 @@
 function validateForm () {
+ var name = document.forms ["contactForm"]["name"].value;
+ var phone = document.forms ["contactForm"]["phone"].value;
+ var email = document.forms ["contactForm"]["email"].value;
+    if (name === "" || phone === "" || email === "") {
+        alert ("Required Field Set Empty!")
+    }
+    else ( alert ("Thanks for contacting me!")) 
     
 }
